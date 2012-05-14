b030028725b692c5da9f4c852b81128202d4ade3
Gregg Lind <gregg.lind@gmail.com>
2012-05-14 12:06:02 -0500

    putative 0.2.  tons of changes.  all of it doesn't quite work yet.
    This has the 'flag' system with callbacks, which I am back
    to disliking and will remove again soon :)
    
    Still needs *tons* of bulletproofing, but some UI bits work.
    
    In particular, no panels (banners only), due to panel drawbacks
    (the are too ephemeral, only have XUL (and not jQ) events,
    are non-discoverable, too non-blocking, etc.)

---
+++ data/js/contentscript.js
@@ -0,0 +1,49 @@
+/* scripts for all pages 
+
+This should run in ADDON SCOPE in a worker 
+
+assume:
+
+jquery, underscore, iCanHaz.js
+*/
+
+$(function() {
+    self.port.on('populatestudies', function(studies){
+        var experiments = $("#experiments");
+        var simplesurveys = $("#simplesurveys");
+        experiments.empty();
+        simplesurveys.empty();
+        
+        $.each(studies, function(key,study){
+            let t = study.config.studytype;
+            switch (t) {
+                case 'simplesurvey':  simplesurveys.append(ich.simplesurvey_tpl(study.config)); break;
+                case 'experiment':  experiments.append(ich.experiment_tpl(study.config)); break;
+            }
+        });
+    });
+});
+
+
+$(function() {
+    self.port.on('datafromstudy', function(studyid,data){
+        var that = $("#collecteddata");
+        that.empty();
+        that.append('<p>data for study: ' + studyid + '</p>');
+        that.append('<p>here is the data...</p>');
+    });
+});
+
+
+$(function(){
+    self.emit("selfmessage","from the contentscript");
+});
+
+
+$(function() {
+    self.port.on('attached', function(message) {
+                         window.alert(message);
+    });
+    window.alert("saw the message");
+});
+
