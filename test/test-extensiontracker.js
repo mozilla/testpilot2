7cc5db947d647cf1512491c195ecbb6b19155d6b
Gregg Lind <gregg.lind@gmail.com>
2012-09-07 10:47:54 -0500

    extension tracker stub and tests

--- test/test-extensiontracker.js
+++ test/test-extensiontracker.js
@@ -12,3 +12,11 @@ exports.test_installed_extensions = function(test){
 	test.fail("TODO: write test for installed_extensions");
 };
 
+
+exports["test extensiontracker persists"] = function(assert){assert.fail("nope")};
+
+exports["test extensiontracker checks what is installed before allowing an uninstall by testpilot"] = function(assert){assert.fail("nope")};
+
+exports["test okay to uninstall if it was yours and no one else needs it"] = function(assert){assert.fail("nope")};
+
+
